3e3853db017ce2cc39d8e879976c7025298a2296
Kasztandor <michal.swoboda@interia.pl>
2022-07-08 11:46:38 +0200

    Pencil and rubber working

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var selected = 0
+var selected = -1
+var canvas = document.getElementsByTagName("canvas")[0]
+var ctx = canvas.getContext("2d");
+var mouseDown = false
+var interval
+var coords = []
+var lastCoords = [];
 
 function selectReload(){
     var selects = document.querySelectorAll(".selected")
@@ -7,8 +13,49 @@ function selectReload(){
     })
     document.querySelectorAll(".tool")[selected].classList.add("selected")
 }
-
 function select(x){
     selected = x
     selectReload()
 }
+
+function draw(p){
+    if (mouseDown){
+        if (selected==0){
+            ctx.beginPath()
+            ctx.strokeStyle = "#000000";
+            ctx.moveTo(p.pageX-61,p.pageY)
+            ctx.lineTo(lastCoords[0]-61, lastCoords[1])
+            ctx.lineWidth = 10
+            ctx.stroke()
+
+            ctx.beginPath()
+            ctx.fillStyle = "#000000";
+            ctx.arc(p.pageX-61, p.pageY, 5, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+        else if (selected==1){
+            ctx.beginPath()
+            ctx.strokeStyle = "#FFFFFF";
+            ctx.moveTo(p.pageX-61,p.pageY)
+            ctx.lineTo(lastCoords[0]-61, lastCoords[1])
+            ctx.lineWidth = 10
+            ctx.stroke()
+
+            ctx.beginPath()
+            ctx.fillStyle = "#FFFFFF";
+            ctx.arc(p.pageX-61, p.pageY, 5, 0, 2 * Math.PI)
+            ctx.fill()
+        }
+    }
+    lastCoords=[p.pageX,p.pageY]
+}
+canvas.addEventListener('pointerdown', (event) => {
+    mouseDown = true
+});
+canvas.addEventListener('pointerup', (event) => {
+    mouseDown = false
+});
+/*canvas.addEventListener('pointerout', (event) => {
+    mouseDown = false
+});*/
+addEventListener('mousemove', draw, false);
